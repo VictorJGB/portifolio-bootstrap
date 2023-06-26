4f7b24803d146cac4783721dd6e2bd21e0bda074
Victor Jerrysson <victorjerrysson@gmail.com>
2023-06-26 02:02:43 -0300

    refactor: small changes on the header links

--- scripts/header.js
+++ scripts/header.js
@@ -1,4 +1,5 @@
 const headerRef = document.getElementById("navBar");
+const nav = document.getElementById("navbarNav");
 
 // Header change color on scroll from top
 window.onscroll = function () {
@@ -7,7 +8,9 @@ window.onscroll = function () {
     document.documentElement.scrollTop >= 200
   ) {
     headerRef.classList.add("nav-bar-active");
+    nav.classList.add("navbarNav-active");
   } else {
     headerRef.classList.remove("nav-bar-active");
+    nav.classList.remove("navbarNav-active");
   }
 };

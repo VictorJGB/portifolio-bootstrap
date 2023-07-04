6a1b622a30278a9408c06450720bb8ca71648af4
Victor Jerrysson <victorjerrysson@gmail.com>
2023-07-04 02:27:00 -0300

    feat: adding the active link logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
 import { onHeaderScroll } from './header.js';
 
 onHeaderScroll();
+
+// on scroll change active link
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('.navbar .container .collapse ul li');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove('active');
+    if (li.classList.contains(current)) {
+      li.classList.add('active');
+    }
+  });
+});

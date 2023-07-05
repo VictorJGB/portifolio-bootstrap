4d54a0350de74dfbe6e66687054bab9b81952f5f
Victor Jerrysson <victorjerrysson@gmail.com>
2023-07-05 13:13:50 -0300

    feat: formMessage handler scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,32 @@
-import { onHeaderScroll } from './header.js';
+// header consts
+const headerRef = document.getElementById('navBar');
+const nav = document.getElementById('navbarNav');
 
-onHeaderScroll();
-
-// on scroll change active link
+// navbar consts
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('.navbar .container .collapse ul li');
+
+// form consts
+const footerForm = document.querySelector('#footerForm');
+const emailInput = document.querySelector('#emailControl');
+const messageInput = document.querySelector('#messageControl');
+var userMessages = [];
+
+// Header change color on scroll from top
+window.onscroll = function () {
+  if (
+    document.body.scrollTop >= 200 ||
+    document.documentElement.scrollTop >= 200
+  ) {
+    headerRef.classList.add('nav-bar-active');
+    nav.classList.add('navbarNav-active');
+  } else {
+    headerRef.classList.remove('nav-bar-active');
+    nav.classList.remove('navbarNav-active');
+  }
+};
+
+// on scroll change active link
 
 window.addEventListener('scroll', () => {
   let current = '';
@@ -23,3 +45,23 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+
+// form messages handler
+function onSubmit() {
+  const emailValue = emailInput.value;
+  const messageValue = messageInput.value;
+
+  let userMessageObject = {
+    email: emailValue,
+    message: messageValue,
+  };
+
+  userMessages.push(userMessageObject);
+
+  localStorage.setItem('userMessage', JSON.stringify(userMessages));
+
+  const getUserMessages = localStorage.getItem('userMessage');
+  const userMessage = JSON.parse(getUserMessages);
+
+  console.log(userMessage);
+}
